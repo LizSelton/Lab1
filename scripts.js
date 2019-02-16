704155da18e26c2f05c4ebdf0ff26b5ca30447ac
LizSelton <selelia@dunwoody.edu>
2019-02-15 22:08:56 -0600

    final commit

--- scripts.js
+++ scripts.js
@@ -39,4 +39,89 @@ request.onload = function () {
 
 request.send();
 
+var myConfig = {
+  type: "pie", 
+  backgroundColor: "#2B313B",
+  plot: {
+    borderColor: "#2B313B",
+    borderWidth: 5,
+    // slice: 90,
+    valueBox: {
+      placement: 'out',
+      text: '%t\n%npv%',
+      fontFamily: "Open Sans"
+    },
+    tooltip:{
+      fontSize: '18',
+      fontFamily: "Open Sans",
+      padding: "5 10",
+      text: ""
+    },
+    animation:{
+     effect: 2, 
+     method: 5,
+     speed: 500,
+     sequence: 1
+   }
+  },
+  source: {
+    text: '/api/forbes400/',
+    fontColor: "#8e99a9",
+    fontFamily: "Open Sans"
+  },
+  title: {
+    fontColor: "#fff",
+    text: 'Forbes Richest Women by Age',
+    align: "left",
+    offsetX: 10,
+    fontFamily: "Open Sans",
+    fontSize: 25
+  },
+  subtitle: {
+    offsetX: 10,
+    offsetY: 10,
+    fontColor: "#8e99a9",
+    fontFamily: "Open Sans",
+    fontSize: "16",
+    text: '',
+    align: "left"
+  },
+  plotarea: {
+    margin: "20 0 0 0"  
+  },
+ series : [
+   {
+     values : [11.38],
+     text: "Age 20-35",
+     backgroundColor: '#50ADF5',
+   },
+   {
+     values: [56.94],
+     text: "Age 36-51",
+     backgroundColor: '#FF7965'
+   },
+   {
+     values: [14.52],
+     text: 'Age 52-66',
+     backgroundColor: '#FFCB45'
+   },
+   {
+     text: 'Age 67-82',
+     values: [9.69],
+     backgroundColor: '#6877e5'
+   },
+   {
+     text: 'Age 83-99',
+     values: [7.48],
+     backgroundColor: '#6FB07F'
+   }
+ ]
+};
 
+zingchart.render({ 
+ id : 'myChart', 
+ data : myConfig, 
+ height: 500, 
+ width: 725
+ 
+});
